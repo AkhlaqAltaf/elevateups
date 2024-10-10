92b314779c8761f6d61fba2b1e0745f984b76836
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2024-10-11 00:34:29 +0500

    MEMBERS FROM DATABASE

--- src/apps/website/urls.py
+++ src/apps/website/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from core import settings
 from src.apps.website.views import WebsiteView
 
 app_name = "website"
@@ -9,3 +11,5 @@ urlpatterns = [
     path('', WebsiteView.as_view(), name='portfolio'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/apps/website/views.py
+++ src/apps/website/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
+from src.apps.accounts.models import CustomUser
+
+
 class WebsiteView(TemplateView):
     template_name = "website/index.html"
+    def get_context_data(self, **kwargs):
+        print("GET TEAM..")
+        team = CustomUser.objects.all()
+        print("TEAM GET ,,",team)
+        return {'team':team}
 
