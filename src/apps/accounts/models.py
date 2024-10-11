ccf79e550c68a82d313433d696ce54827453b915
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2024-10-11 16:19:18 +0500

    BLOGS CREATED

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -7,21 +7,15 @@ class CustomUser(models.Model):
         ('developer', 'Developer'),
         ('member', 'Member')
     ]
-    MEMBER_STATUS_CHOICES = [
-        ('CTO', 'Chief Technology Officer'),
-        ('CEO', 'Chief Executive Officer'),
-        ('CFO', 'Chief Financial Officer'),
-        ('project_manager', 'Project Manager'),
-        ('team_leader', 'Team Leader')
-    ]
+
 
     full_name = models.CharField(max_length=100)
     email = models.EmailField(blank=True,null=True)
     phone = models.CharField(max_length=100 , blank=True, null=True)
     experience_detail  = models.TextField(blank=True,null=True)
     links = models.TextField(blank=True, null=True)
-    member_status = models.CharField(max_length=20, choices=MEMBER_STATUS_CHOICES, blank=True, null=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='client')
+    member_status = models.CharField(max_length=20, blank=True, null=True)
     status_description = models.TextField(blank=False,null=True)
     profile_picture = models.ImageField(blank=True,null=True,upload_to='user/')
     is_display_on_site = models.BooleanField(default=False,blank=True,null=True)
--- src/apps/website/urls.py
+++ src/apps/website/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.urls import path
 
 from core import settings
-from src.apps.website.views import WebsiteView
+from src.apps.website.views import WebsiteView, BlogDetailView, MoreBlogsView
 
 app_name = "website"
 
 urlpatterns = [
     path('', WebsiteView.as_view(), name='portfolio'),
-
+    path('blog/<int:id>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('more-blogs/', MoreBlogsView.as_view(), name='more_blogs'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
