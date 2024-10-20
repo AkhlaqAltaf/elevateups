bb3ff97300694e2c1a73b9747100769852439f2a
AkhlaqAltaf <akhlaqaltaf4@gmail.com>
2024-10-20 06:37:01 +0500

    RESOLVED

--- src/apps/website/views.py
+++ src/apps/website/views.py
@@ -50,14 +50,6 @@ class SiteMap(Sitemap):
 
 # CONTACT US VIEW
 
-# views.py
-
-from django.core.mail import send_mail
-from django.shortcuts import render
-from .forms import ContactForm
-from django.conf import settings
-
-# views.py
 
 from django.http import JsonResponse
 from django.core.mail import send_mail
